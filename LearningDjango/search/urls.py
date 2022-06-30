7899d92134b845db73831905bd5c62bb34f052c6
codyswanner <108250848+codyswanner@users.noreply.github.com>
2022-06-30 18:10:49 -0500

    Add files via upload

--- LearningDjango/search/urls.py
+++ LearningDjango/search/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'search'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('advanced_search', views.advanced_search, name='advanced_search'),
     path('results', views.search_results, name='search_results')
 ]
--- LearningDjango/search/views.py
+++ LearningDjango/search/views.py
@@ -13,40 +13,48 @@ def index(request):
     return render(request, 'search/index.html')
 
 
+def advanced_search(request):
+    return render(request, 'search/advanced_search.html', {})
+
+
 def search_results(request):
-    search_by = request.POST['search_by']
-    search_term = request.POST['search_term']
-    # empty results set here isn't necessary, just makes PyCharm warning shut up
-    results = []
-    # Using * overrides search_by and shows all data (also displays "searched * by *")
-    if search_term == "*":
-        results = Books.objects.all()
-        search_by = "*"
-    else:
-        if search_by == "all":
-            results = Books.objects.filter(
-                Q(author_first__icontains=search_term) |
-                Q(author_last__icontains=search_term) |
-                Q(author_middle__icontains=search_term) |
-                Q(title__icontains=search_term) |
-                Q(genre_1__icontains=search_term) |
-                Q(genre_2__icontains=search_term) |
-                Q(genre_3__icontains=search_term) |
-                Q(shelf__icontains=search_term) |
-                Q(language__icontains=search_term))
-        elif search_by == "title":
-            results = Books.objects.filter(title__icontains=search_term)
-        elif search_by == "author":
-            results = Books.objects.filter(Q(author_first__icontains=search_term) | Q(author_last__icontains=search_term))
-        elif search_by == "genre":
-            results = Books.objects.filter(Q(genre_1__icontains=search_term) | Q(genre_2__icontains=search_term) | Q(genre_3__icontains=search_term))
-        elif search_by == "shelf":
-            results = Books.objects.filter(shelf__icontains=search_term)
-        elif search_by == "language":
-            results = Books.objects.filter(language__contains=search_term)
-        # Add else statement that brings up error page
-    context = {'search_term': search_term,
-               'results': results,
-               'search_by': search_by,
-               }
-    return render(request, 'search/results_update.html', context)
+    if request.POST['search_type'] == "default_search":
+        search_by = request.POST['search_by']
+        search_term = request.POST['search_term']
+        # empty results set here isn't necessary, just makes PyCharm warning shut up
+        results = []
+        # Using * overrides search_by and shows all data (also displays "searched * by *")
+        if search_term == "*":
+            results = Books.objects.all()
+            search_by = "*"
+        else:
+            if search_by == "all":
+                results = Books.objects.filter(
+                    Q(author_first__icontains=search_term) |
+                    Q(author_last__icontains=search_term) |
+                    Q(author_middle__icontains=search_term) |
+                    Q(title__icontains=search_term) |
+                    Q(genre_1__icontains=search_term) |
+                    Q(genre_2__icontains=search_term) |
+                    Q(genre_3__icontains=search_term) |
+                    Q(shelf__icontains=search_term) |
+                    Q(language__icontains=search_term))
+            elif search_by == "title":
+                results = Books.objects.filter(title__icontains=search_term)
+            elif search_by == "author":
+                results = Books.objects.filter(Q(author_first__icontains=search_term) | Q(author_last__icontains=search_term))
+            elif search_by == "genre":
+                results = Books.objects.filter(Q(genre_1__icontains=search_term) | Q(genre_2__icontains=search_term) | Q(genre_3__icontains=search_term))
+            elif search_by == "shelf":
+                results = Books.objects.filter(shelf__icontains=search_term)
+            elif search_by == "language":
+                results = Books.objects.filter(language__contains=search_term)
+            # Add else statement that brings up error page
+        context = {'search_term': search_term,
+                   'results': results,
+                   'search_by': search_by,
+                   }
+        return render(request, 'search/results_update.html', context)
+    elif request.POST['search_type'] == "advanced_search":
+        return render(request, 'search/results_update.html', {})
+
