eeb9a7d6576eb936c76e532af2cc0b72046e597f
codyswanner <108250848+codyswanner@users.noreply.github.com>
2022-07-11 11:55:06 -0500

    Revert "Add files via upload"
    This reverts commit 83030405f6c45a815a71dcf3e6a0ccd42f0a9a24.

--- LearningDjango/urls.py
+++ LearningDjango/urls.py
@@ -1,11 +1,23 @@
-from django.urls import path
-from . import views
+"""LearningDjango URL Configuration
 
-app_name = 'search'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('advanced_search', views.advanced_search, name='advanced_search'),
-    path('results', views.search_results, name='search_results'),
-    path('new_book_form', views.new_book_form, name='new_book_form'),
-    path('new_book_confirmation', views.new_book_confirmation, name='new_book_confirmation')
+    path('admin/', admin.site.urls),
+    path('search/', include('search.urls')),
+    path('', include('home.urls'))
 ]
