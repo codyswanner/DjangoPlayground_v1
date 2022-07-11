83030405f6c45a815a71dcf3e6a0ccd42f0a9a24
codyswanner <108250848+codyswanner@users.noreply.github.com>
2022-07-11 11:44:21 -0500

    Add files via upload
    Don't think anything changed with these, but never hurts to update just in case

---
+++ LearningDjango/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+import re
+
+
+# Create your models here.
+
+# The titlecase function *sort of* corrects capitalization mistakes by users.  See notes inside function.
+def titlecase(s):
+    # This step titlecases the first letter of all words, including trivial words like "and", "the", "of", etc.
+    all_titled = re.sub(
+        r"[A-Za-z]+('[A-Za-z]+)?",
+        lambda word: word.group(0).capitalize(),
+        s)
+
+    # This step *sort of* lowercases the trivial words.  However, in an effort to avoid inadvertent capturing,
+    # the regexp specifies a whitespace on each side of the word.  This causes issues with situations like " and the "
+    # because there is only one whitespace in the middle, and therefore only one match, so it will return " and The ",
+    # which is incorrect.  Also, worth noting that the all_titled string will have all capitalized words, hence the
+    # capitalized words in the capturing group.
+    # TODO: make a better regexp for this
+    trivials_lowered = re.sub(
+        r"\s(The|And|Of|For|In|A|To|An|By|On|That|But|Yet|So|Nor|Or|As|At)\s",
+        lambda word: word.group(0).lower(),
+        all_titled)
+
+    return trivials_lowered
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=70, null=False)
+    author_first = models.CharField(max_length=30, null=False)
+    author_middle = models.CharField(max_length=30, null=True, blank=True)
+    author_last = models.CharField(max_length=30, null=False)
+    genre_1 = models.CharField(max_length=35, null=False, choices=[
+        ("Realistic Fiction", "Realistic Fiction"),
+        ("Literary Fiction", "Literary Fiction"),
+        ("Mystery / Detective Fiction", "Mystery / Detective Fiction"),
+        ("Romance", "Romance"),
+        ("Historical Fiction", "Historical Fiction"),
+        ("Thriller / Horror", "Thriller / Horror"),
+        ("Science Fiction", "Science Fiction"),
+        ("Fantasy", "Fantasy"),
+        ("Children's Books / Early Readers", "Children's Books / Early Readers"),
+    ])
+    genre_2 = models.CharField(max_length=35, null=True, blank=True, choices=[
+        ("Realistic Fiction", "Realistic Fiction"),
+        ("Literary Fiction", "Literary Fiction"),
+        ("Mystery / Detective Fiction", "Mystery / Detective Fiction"),
+        ("Romance", "Romance"),
+        ("Historical Fiction", "Historical Fiction"),
+        ("Thriller / Horror", "Thriller / Horror"),
+        ("Science Fiction", "Science Fiction"),
+        ("Fantasy", "Fantasy"),
+        ("Children's Books / Early Readers", "Children's Books / Early Readers"),
+        ("Young Adult Fiction", "Young Adult Fiction"),
+    ])
+    genre_3 = models.CharField(max_length=35, null=True, blank=True, choices=[
+        ("Realistic Fiction", "Realistic Fiction"),
+        ("Literary Fiction", "Literary Fiction"),
+        ("Mystery / Detective Fiction", "Mystery / Detective Fiction"),
+        ("Romance", "Romance"),
+        ("Historical Fiction", "Historical Fiction"),
+        ("Thriller / Horror", "Thriller / Horror"),
+        ("Science Fiction", "Science Fiction"),
+        ("Fantasy", "Fantasy"),
+        ("Children's Books / Early Readers", "Children's Books / Early Readers"),
+        ("Young Adult Fiction", "Young Adult Fiction"),
+    ])
+    language = models.CharField(max_length=20,
+                                choices=[("English", "English"), ("Spanish / Español", "Spanish / Español")])
+    shelf = models.CharField(max_length=10, null=False, default="0", choices=[
+        ("1A", "1A"), ("1B", "1B"), ("1C", "1C"),
+        ("2A", "2A"), ("2B", "2B"), ("2C", "2C"),
+        ("3A", "3A"), ("3B", "3B"), ("3C", "3C"),
+        ("4A", "4A"), ("4B", "4B"), ("4C", "4C"),
+        ("5A", "5A"), ("5B", "5B"), ("5C", "5C"),
+        ("6A", "6A"), ("6B", "6B"), ("6C", "6C"),
+        ("7A", "7A"), ("7B", "7B"), ("7C", "7C"),
+        ("8A", "8A"), ("8B", "8B"), ("8C", "8C"),
+        ("9A", "9A"), ("9B", "9B"), ("9C", "9C"),
+        ("10A", "10A"), ("10B", "10B"), ("10C", "10C"),
+        ("11A", "11A"), ("11B", "11B"), ("11C", "11C"),
+        ("12A", "12A"), ("12B", "12B"), ("12C", "12C"),
+        ("13A", "13A"), ("13B", "13B"), ("13C", "13C"),
+        ("14A", "14A"), ("14B", "14B"), ("14C", "14C"),
+        ("15A", "15A"), ("15B", "15B"), ("15C", "15C"),
+        ("16A", "16A"), ("16B", "16B"), ("16C", "16C"),
+        ("0", "0")
+    ])
+
+    def __str__(self):
+        return self.title
--- LearningDjango/urls.py
+++ LearningDjango/urls.py
@@ -1,23 +1,11 @@
-"""LearningDjango URL Configuration
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
+app_name = 'search'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('search/', include('search.urls')),
-    path('', include('home.urls'))
+    path('', views.index, name="index"),
+    path('advanced_search', views.advanced_search, name='advanced_search'),
+    path('results', views.search_results, name='search_results'),
+    path('new_book_form', views.new_book_form, name='new_book_form'),
+    path('new_book_confirmation', views.new_book_confirmation, name='new_book_confirmation')
 ]
